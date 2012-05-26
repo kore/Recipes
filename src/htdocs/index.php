968121cec6d27d11c73c4791f6082e004f16ae2a
Kore Nordmann <mail@kore-nordmann.de>
2012-05-26 07:29:05 +0000

    Added: Basic index.php and bootstrap.php

---
+++ src/htdocs/index.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * This file is part of recipes
+ *
+ * This file is part of recipes.
+ *
+ * recipes is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3 of the License.
+ *
+ * recipes is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with recipes; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * @package Core
+ * @version $Revision: 1469 $
+ * @license http://www.gnu.org/licenses/gpl-3.0.txt GPL
+ */
+
+namespace Recipes;
+use Qafoo\RMF;
+
+require __DIR__ . '/../main/Recipes/bootstrap.php';
+$dic = new DIC\Base();
+$dic->environment = 'development';
+
+$dispatcher = new RMF\Dispatcher\Simple(
+    new RMF\Router\Regexp( array(
+        '(^/$)' => array(
+            'GET'  => array( $dic->controller, 'showOverview' ),
+        ),
+    ) ),
+    $dic->view
+);
+
+$request = new RMF\Request\HTTP();
+$request->addHandler( 'body', new RMF\Request\PropertyHandler\PostBody() );
+$request->addHandler( 'session', new RMF\Request\PropertyHandler\Session() );
+
+$dispatcher->dispatch( $request );
+
---
+++ src/main/Recipes/bootstrap.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * This file is part of recipes
+ *
+ * This file is part of recipes.
+ *
+ * recipes is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3 of the License.
+ *
+ * recipes is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with recipes; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * @package Core
+ * @version $Revision: 1469 $
+ * @license http://www.gnu.org/licenses/gpl-3.0.txt GPL
+ */
+
+namespace Recipes;
+
+// @codeCoverageIgnoreStart
+// @codingStandardsIgnoreStart
+
+require __DIR__ . '/../../library/autoload.php';
+
+spl_autoload_register(
+    function ( $class )
+    {
+        if ( 0 === strpos( $class, __NAMESPACE__ ) )
+        {
+            include __DIR__ . '/../../' . strtr( $class, '\\', '/' ) . '.php';
+        }
+    }
+);
+
+// @codingStandardsIgnoreEnd
+// @codeCoverageIgnoreEnd
