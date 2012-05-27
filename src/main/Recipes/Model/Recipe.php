0dd2d4754c6d52ac508110f413067710a742845b
Kore Nordmann <mail@kore-nordmann.de>
2012-05-27 09:39:37 +0000

    Use foreach instead of map relying on PHP 5.4

--- src/main/Recipes/Model/Recipe.php
+++ src/main/Recipes/Model/Recipe.php
@@ -123,20 +123,16 @@ class Recipe extends Model
     public function getLatest( $count = 10 )
     {
         $recipes = $this->gateway->getLatest( $count );
-        $gateway = $this->gateway;
-        return array_map( function( $doc ) use ( $gateway )
+        foreach ( $recipes as $nr => $doc )
+        {
+            $recipes[$nr] = new Recipe( $this->gateway, $doc['_id'] );
+            foreach ( $doc as $property => $value )
             {
-                $recipe = new Recipe( $gateway, $doc['_id'] );
+                $recipes[$nr]->properties[$property] = $value;
+            }
+        }
 
-                foreach ( $doc as $property => $value )
-                {
-                    $recipe->properties[$property] = $value;
-                }
-
-                return $recipe;
-            },
-            $recipes
-        );
+        return $recipes;
     }
 
     /**
