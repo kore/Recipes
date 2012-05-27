e97d0c0240e78aadbfa776569bacafd5decfa541
Kore Nordmann <mail@kore-nordmann.de>
2012-05-27 20:36:56 +0000

    Show attached images in colorbox

--- src/htdocs/scripts/recipe.js
+++ src/htdocs/scripts/recipe.js
@@ -2,4 +2,13 @@ $( document ).ready( function()
 {
     $( "textarea" ).elastic();
     $( "form.protect" ).protect( 'Änderungen am Rezept werden dabei verloren gehen.' );
+
+    $( "ul.images a" ).unbind( "click" );
+    $( "ul.images a" ).colorbox( {
+        rel: "thumbnail",
+        scale: true,
+        maxWidth: "80%",
+        maxHeight: "80%"
+    } );
+
 } );
