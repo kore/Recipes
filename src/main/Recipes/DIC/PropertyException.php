6c5e0091dee2ba0d7c8dc800f9394d087e004f4e
Kore Nordmann <mail@kore-nordmann.de>
2012-06-23 12:30:10 +0200

    Removed unknown interfaces

--- src/main/Recipes/DIC/PropertyException.php
+++ src/main/Recipes/DIC/PropertyException.php
@@ -7,14 +7,12 @@
 
 namespace Puppeteer\Commons\DIC;
 
-use \Puppeteer\Commons\Throwable;
-
 /**
  * This exception will be thrown if a property does not exist in the DIC.
  *
  * @version $Revision$
  */
-class PropertyException extends \InvalidArgumentException implements Throwable
+class PropertyException extends \InvalidArgumentException
 {
 
 }
--- src/main/Recipes/DIC/RuntimeException.php
+++ src/main/Recipes/DIC/RuntimeException.php
@@ -7,14 +7,12 @@
 
 namespace Puppeteer\Commons\DIC;
 
-use \Puppeteer\Commons\Throwable;
-
 /**
  * This exception will be thrown if something bad happens inside the DIC.
  *
  * @version $Revision$
  */
-class RuntimeException extends \RuntimeException implements Throwable
+class RuntimeException extends \RuntimeException
 {
 
 }
