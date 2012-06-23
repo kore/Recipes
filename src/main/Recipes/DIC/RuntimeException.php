0d63d65b29f1b4257718df0b27443aabedcc11ce
Kore Nordmann <mail@kore-nordmann.de>
2012-06-23 12:31:33 +0200

    Fixed exception header

--- src/main/Recipes/DIC/RuntimeException.php
+++ src/main/Recipes/DIC/RuntimeException.php
@@ -1,11 +1,26 @@
 <?php
 /**
- * This file is part of the Puppeteer Commons Component.
+ * This file is part of recipes.
  *
- * @version $Revision$
+ * recipes is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3 of the License.
+ *
+ * recipes is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with recipes; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * @package Core
+ * @version $Revision: 1469 $
+ * @license http://www.gnu.org/licenses/gpl-3.0.txt GPL
  */
 
-namespace Puppeteer\Commons\DIC;
+namespace Recipes\DIC;
 
 /**
  * This exception will be thrown if something bad happens inside the DIC.
